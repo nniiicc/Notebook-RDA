2cb70e809512d8aab583d46b2c8618bdbde8c251
Nic <nmweber@illinois.edu>
2015-04-16 15:30:38 -0500

    Update RDA-Plots.R

--- Code/RDA-Plots.R
+++ Code/RDA-Plots.R
@@ -1,3 +1,4 @@
+#rda Org Type plot
 p<-ggplot(rda,
        aes(x=reorder(Organization.Type,Organization.Type,
                      function(x)-length(x)))) +
@@ -5,9 +6,11 @@ p<-ggplot(rda,
 
 p + labs(list(title = "Org. Type Working Group Members in RDA", x = "Organizational Types", y = "Members"))
 
-> h<-ggplot(rda,
-+        aes(x=reorder(Title,Title,
-+                      function(x)-length(x)))) +
-+     geom_bar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-> h
-> h + labs(list(title = "Titles of Working Group Members in RDA", x = "Titles", y = "Members"))
+#rda Title plot
+
+h<-ggplot(rda,
+        aes(x=reorder(Title,Title,
+                      function(x)-length(x)))) +
+     geom_bar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+     
+ h + labs(list(title = "Titles of Working Group Members in RDA", x = "Titles", y = "Members"))
