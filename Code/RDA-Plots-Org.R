f7d0840c62c5e4107bb14595aa9b5abe65d972e1
Nic <nmweber@illinois.edu>
2015-04-16 15:31:05 -0500

    Update and rename RDA-Plots.R to RDA-Plots-Org.R

--- Code/RDA-Plots-Org.R
+++ Code/RDA-Plots-Org.R
@@ -5,12 +5,3 @@ p<-ggplot(rda,
     geom_bar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 p + labs(list(title = "Org. Type Working Group Members in RDA", x = "Organizational Types", y = "Members"))
-
-#rda Title plot
-
-h<-ggplot(rda,
-        aes(x=reorder(Title,Title,
-                      function(x)-length(x)))) +
-     geom_bar() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-     
- h + labs(list(title = "Titles of Working Group Members in RDA", x = "Titles", y = "Members"))
