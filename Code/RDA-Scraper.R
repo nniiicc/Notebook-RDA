7632236bdfac14e58e2570d2fd33ab7d3471973f
Nic <nmweber@illinois.edu>
2015-04-16 15:29:09 -0500

    Update RDA-Scraper.R

--- Code/RDA-Scraper.R
+++ Code/RDA-Scraper.R
@@ -1,6 +1,6 @@
 library('RCurl')
 library('rjson')
-json <- getURL('https://www.kimonolabs.com/api/778e6bfc?apikey=4fd371c8abf01aad82b0e4f9682baa58')
+json <- getURL('https://www.kimonolabs.com/api/778e6bfc?apikey=4fd371c8abf01aad82b0e4f9682baa58') ##Call API, and fetch scraped data
 obj <- fromJSON(json)
 print(obj) 
 
